d0938fb202c82c5a3dba307b238d983a9f6c360a
Bjorn Kristinsson <bjorn@bjornkri.com>
2011-06-12 14:46:47 +0200

    Added setup.py. Tidied up.

---
+++ README.txt
@@ -0,0 +1,3 @@
+A wrapper around a rewrite of a suite of applications I've built and use on various websites. An attempt to make them more reusable.
+
+In this first release, a simple photo management application, ``bilskirnir``.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Asgardur',
+    version='0.1',
+    packages=['asgardur'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
